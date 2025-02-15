ccf66a5d6ba7d5694b09d0a4ccff15a8ee64a12d
pjunstall <pjtunstall@gmail.com>
2025-02-15 08:16:08 +0000

    echo: parse special characters

--- src/commands/echo.rs
+++ src/commands/echo.rs
@@ -7,42 +7,8 @@ pub fn echo(input: &Vec<String>) -> Result<String, String> {
 
     let mut output = input[1].clone();
 
-    output = output.replace(
-        "$USER",
-        &env::var("USER").unwrap_or_else(|_| "unknown".to_string()),
-    );
-    output = output.replace(
-        "$HOSTNAME",
-        &env::var("HOSTNAME").unwrap_or_else(|_| "unknown".to_string()),
-    );
-    output = output.replace(
-        "$PID",
-        &env::var("PID").unwrap_or_else(|_| "unknown".to_string()),
-    );
-    output = output.replace(
-        "$PATH",
-        &env::var("PATH").unwrap_or_else(|_| "unknown".to_string()),
-    );
-    output = output.replace(
-        "$SHELL",
-        &env::var("SHELL").unwrap_or_else(|_| "unknown".to_string()),
-    );
-    output = output.replace(
-        "$UMASK",
-        &env::var("UMASK").unwrap_or_else(|_| "unknown".to_string()),
-    );
-    output = output.replace(
-        "$HOME",
-        &env::var("HOME").unwrap_or_else(|_| "unknown".to_string()),
-    );
-    output = output.replace(
-        "$LANG",
-        &env::var("LANG").unwrap_or_else(|_| "unknown".to_string()),
-    );
-    output = output.replace(
-        "$TERM",
-        &env::var("TERM").unwrap_or_else(|_| "unknown".to_string()),
-    );
+    output = parse_special_characters(&output);
+    parse_environment_variables(&mut output);
 
     if input.len() > 2 {
         for i in 2..input.len() {
@@ -53,4 +19,54 @@ pub fn echo(input: &Vec<String>) -> Result<String, String> {
 
     output.push('\n');
     Ok(output)
+}
+
+fn parse_special_characters(output: &str) -> String {
+    output
+        .replace(r"\n", "\n")
+        .replace(r"\t", "\t")
+        .replace(r"\v", "\x0b")
+        .replace(r"\a", "\x07")
+        .replace(r"\b", "\x08")
+        .replace(r"\r", "\r")
+        .replace(r"\\", "\\")
+}
+
+fn parse_environment_variables(output: &mut String) {
+    *output = output.replace(
+        "$USER",
+        &env::var("USER").unwrap_or_else(|_| "unknown".to_string()),
+    );
+    *output = output.replace(
+        "$HOSTNAME",
+        &env::var("HOSTNAME").unwrap_or_else(|_| "unknown".to_string()),
+    );
+    *output = output.replace(
+        "$PID",
+        &env::var("PID").unwrap_or_else(|_| "unknown".to_string()),
+    );
+    *output = output.replace(
+        "$PATH",
+        &env::var("PATH").unwrap_or_else(|_| "unknown".to_string()),
+    );
+    *output = output.replace(
+        "$SHELL",
+        &env::var("SHELL").unwrap_or_else(|_| "unknown".to_string()),
+    );
+    *output = output.replace(
+        "$UMASK",
+        &env::var("UMASK").unwrap_or_else(|_| "unknown".to_string()),
+    );
+    *output = output.replace(
+        "$HOME",
+        &env::var("HOME").unwrap_or_else(|_| "unknown".to_string()),
+    );
+    *output = output.replace(
+        "$LANG",
+        &env::var("LANG").unwrap_or_else(|_| "unknown".to_string()),
+    );
+    *output = output.replace(
+        "$TERM",
+        &env::var("TERM").unwrap_or_else(|_| "unknown".to_string()),
+    );
 }
