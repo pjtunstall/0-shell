0dc131f8f469f92ebc5f32cd884cec45093395f4
pjunstall <pjtunstall@gmail.com>
2025-02-18 19:41:03 +0000

    for multiple files and directories, plus test

--- src/commands/rm.rs
+++ src/commands/rm.rs
@@ -1,5 +1,7 @@
 use std::{fs, path::Path};
 
+const USAGE: &str = "Usage: rm [-r] FILE...";
+
 pub fn rm(input: &Vec<String>) -> Result<String, String> {
     debug_assert!(!input.is_empty(), "Input for `rm` should not be empty");
     debug_assert!(
@@ -7,6 +9,46 @@ pub fn rm(input: &Vec<String>) -> Result<String, String> {
         "Input for `{}` should not be passed to `rm`",
         input[0]
     );
+
+    if input.len() < 2 {
+        return Err(format!("not enough arguments\n{}", USAGE).to_string());
+    }
+
+    if input[1] == "-r" {
+        if input.len() < 3 {
+            return Err(format!("not enough arguments\n{}", USAGE).to_string());
+        }
+
+        let mut errors: Vec<Result<(), String>> = Vec::new();
+        let mut i: u32 = 0;
+        for arg in input[2..].iter() {
+            let path = Path::new(&arg);
+
+            let cmd = if i == 0 { "" } else { "rm: " };
+            i += 1;
+
+            if path.is_dir() {
+                _ = fs::remove_dir_all(path).map_err(|err| {
+                    errors.push(Err(format!("{}{}: {}", cmd, arg, err)));
+                });
+            } else {
+                _ = fs::remove_file(path).map_err(|err| {
+                    errors.push(Err(format!("{}{}: {}", cmd, arg, err)));
+                });
+            }
+        }
+
+        if !errors.is_empty() {
+            let error_messages = errors
+                .into_iter()
+                .filter_map(|e| e.err())
+                .collect::<Vec<String>>()
+                .join("\n");
+            return Err(error_messages);
+        }
+
+        return Ok(String::new());
+    }
 
     let mut errors: Vec<Result<(), String>> = Vec::new();
     let mut i: u32 = 0;
@@ -25,7 +67,7 @@ pub fn rm(input: &Vec<String>) -> Result<String, String> {
     if !errors.is_empty() {
         let error_messages = errors
             .into_iter()
-            .filter_map(|e| e.err()) // Extracts the `String` from the `Err` variant
+            .filter_map(|e| e.err())
             .collect::<Vec<String>>()
             .join("\n");
         return Err(error_messages);
@@ -123,6 +165,35 @@ mod tests {
         assert!(
             Path::new(dir2).exists(),
             "Directory should not have been removed"
+        );
+    }
+
+    #[test]
+    fn test_rm_recursive() {
+        let file_store = TempStore::new();
+        let file1 = &file_store.source;
+        let file2 = &file_store.target;
+
+        let dir_store = TempStore::new();
+        let dir2 = &dir_store.source;
+        let dir1 = &dir_store.target;
+
+        fs::create_dir(dir1.to_string()).expect("Failed to create test directory");
+        fs::create_dir(format!("{}/{}", dir1, dir2).to_string())
+            .expect("Failed to create test directory");
+
+        fs::write(format!("{}/{}", dir1, file1).to_string(), "")
+            .expect("Failed to create test file");
+        fs::write(format!("{}/{}/{}", dir1, dir2, file2).to_string(), "")
+            .expect("Failed to create test file");
+
+        let input = vec!["rm".to_string(), "-r".to_string(), dir1.to_string()];
+        let result = rm(&input);
+
+        assert!(result.is_ok(), "`rm` failed: {:?}", result.err());
+        assert!(
+            !Path::new(dir1).exists(),
+            "Directory should have been removed"
         );
     }
 }
